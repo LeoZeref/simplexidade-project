7147f7c07a381e7c8b09e9f821a065e2cf2e60bd
Mônica Colomera <colomeramonica@gmail.com>
2019-05-05 16:31:19 -0300

    Centralizar botão

--- simplex.js
+++ simplex.js
@@ -179,7 +179,7 @@ function firstPhase(){
 		generateRestrictions(quantDec,quantRes);
 
 		//adds a button that calls the second phase of the process
-		$("#inputValues").append('<div id="buttons" class="row"><div class="col-md-6"><button id="solveSimplex" onclick="solveSimplex('+quantDec+','+quantRes+',1)" class="btn btn-primary">Solução direta</button></div></div>');
+		$("#inputValues").append('<div id="buttons" class="row"><div class="col-md-12 col-lg-12"><button id="solveSimplex" align="center" onclick="solveSimplex('+quantDec+','+quantRes+',1)" class="btn btn-primary">Solução</button></div></div>');
 
 		$(".container").append('<div id="solution" class="row"></div>')
 		$(".container").append('<br><div class="row"><div id="results" class="col-md"></div></div>');
